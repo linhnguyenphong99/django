f554327fe688d88421e1889198f156eb0e26aa6c
Nguyen Linh <linh.nguyenphong99@gmail.com>
2025-04-02 09:14:19 +0700

    add style

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -117,11 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Thư mục chứa file tĩnh khi collectstatic
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')  # Thư mục chứa file tĩnh trong quá trình phát triển
-]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
